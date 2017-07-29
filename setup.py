7b51481810e9af12306222fad860fbdd547cfd7d
Holger Mueller <zarath@gmx.de>
2017-07-29 17:46:25 +0200

    Change module arguments
      Redis connection settings are now grouped together in a dictionary.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'wishbone_input_redis'
-VERSION = '0.2.0'
+VERSION = '0.3.0'
 
 install_requires = [
     'wishbone>=2.1.1',
--- wishbone_input_redis/redisin.py
+++ wishbone_input_redis/redisin.py
@@ -25,13 +25,12 @@ class RedisIn(Actor):
 
     Parameters:
 
-        - host(str)("localhost")
-           | Redis hostname
-        - port(int)(6379)
-           | Redis port
-        - database(int)(0)
-           | Index of db to use
-        - key(str)("queue")
+        - redisdb(dict){
+            host(str): ("localhost"),  # Redis hostname
+            port(int): (6379),         # Redis port
+            database(int): (0),        # Index of db to use
+
+        - queue(str)("wishbone.in")
            | name of queue to pop data from
 
     Queues:
@@ -40,33 +39,33 @@ class RedisIn(Actor):
            |  Data coming from the outside world.
     '''
 
-    #pylint: disable=too-many-arguments
-    def __init__(self, actor_config,
-                 host="localhost", port=6379, database=0, key="queue"):
+    def __init__(self, actor_config, redisdb=None, queue="wishbone.in"):
         Actor.__init__(self, actor_config)
-
-        self.host = host
-        self.port = port
-        self.rdb = database
-        self.key = key
-
+        self.rdb = {
+            'host': 'localhost',
+            'port': 6379,
+            'database': 0,
+        }
+        if redisdb:
+            self.rdb.update(redis)
+        self.queue = queue
         self.pool.createQueue("outbox")
 
     def preHook(self):
         '''Sets up redis connection'''
-        conn = redis.Redis(host=self.host, port=self.port)
-        conn.execute_command("SELECT " + str(self.rdb))
+        conn = redis.Redis(host=self.rdb['host'], port=self.rdb['port'])
+        conn.execute_command("SELECT " + str(self.rdb['database']))
 
-        self.logging.info('Connection to %s created.' % (self.host))
-        self.sendToBackground(self.drain, conn, self.key)
+        self.logging.info('Connection to %s created.' % (self.rdb['host']))
+        self.sendToBackground(self.drain, conn, self.queue)
 
-    def drain(self, connection, key):
+    def drain(self, connection, queue):
         '''Reads the redis queue.'''
 
         self.logging.info('Started.')
 
         while self.loop():
-            line = connection.rpop(key)
+            line = connection.rpop(queue)
             if not line:
                 sleep(0.1)
             else:
