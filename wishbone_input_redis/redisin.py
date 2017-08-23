047c285fe602baa66d8f019ee9be5d3cd37366dc
Holger Mueller <hom@cm4all.com>
2017-08-23 12:53:40 +0200

    Cheanged redis db config back to kwargs
       It seems wishbone cannot handle dictionaries in its module args.
       At least not with python3

--- wishbone_input_redis/redisin.py
+++ wishbone_input_redis/redisin.py
@@ -25,11 +25,12 @@ class RedisIn(Actor):
 
     Parameters:
 
-        - redisdb(dict){
-            host(str): ("localhost"),  # Redis hostname
-            port(int): (6379),         # Redis port
-            database(int): (0),        # Index of db to use
-
+        - redis_host(str)("localhost")
+           |  Redis hostname
+        - redis_port(int)(6379)
+           | Redis port
+        - redis_db(int)(0)
+           | Index of db to use
         - queue(str)("wishbone.in")
            | name of queue to pop data from
 
@@ -39,24 +40,22 @@ class RedisIn(Actor):
            |  Data coming from the outside world.
     '''
 
-    def __init__(self, actor_config, redisdb=None, queue="wishbone.in"):
+    def __init__(self, actor_config,
+                 redis_host="localhost", redis_port=6379, redis_db=0,
+                 queue="wishbone.in"):
         Actor.__init__(self, actor_config)
-        self.rdb = {
-            'host': 'localhost',
-            'port': 6379,
-            'database': 0,
-        }
-        if redisdb:
-            self.rdb.update(redis)
+        self.redis_host = redis_host
+        self.redis_port = redis_port
+        self.redis_db = redis_db
         self.queue = queue
         self.pool.createQueue("outbox")
 
     def preHook(self):
         '''Sets up redis connection'''
-        conn = redis.Redis(host=self.rdb['host'], port=self.rdb['port'])
-        conn.execute_command("SELECT " + str(self.rdb['database']))
+        conn = redis.Redis(host=self.redis_host, port=self.redis_port)
+        conn.execute_command("SELECT " + str(self.redis_db))
 
-        self.logging.info('Connection to %s created.' % (self.rdb['host']))
+        self.logging.info('Connection to %s created.' % (self.redis_host))
         self.sendToBackground(self.drain, conn, self.queue)
 
     def drain(self, connection, queue):
